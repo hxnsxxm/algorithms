d4ae15eeb49935a7f159e7b3bcc137a7872a4983
hxnsxxm <hansaemj20@gmail.com>
2024-02-06 17:17:54 +0900

    [PGS] 순자 문자열과 영단어 / 레벨 1

---
+++ programmers/level1/kakao_숫자문자열과영단어.java
@@ -0,0 +1,19 @@
+public class kakao_숫자문자열과영단어 {
+
+    public int solution(String s) {
+        int answer = 0;
+
+        String[][] nums = {
+                {"zero", "0"}, {"one", "1"}, {"two", "2"}, {"three", "3"}, {"four", "4"},
+                {"five", "5"}, {"six", "6"}, {"seven", "7"}, {"eight", "8"}, {"nine", "9"}
+        };
+
+        for (String[] num : nums) {
+            s = s.replaceAll(num[0], num[1]);
+        }
+
+        answer = Integer.parseInt(s);
+
+        return answer;
+    }
+}
