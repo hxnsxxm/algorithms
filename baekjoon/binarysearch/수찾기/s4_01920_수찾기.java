7d1d8c1e908e03b847695e79c74aae3e4c944cd9
hansaem <hansaemj20@gmail.com>
2024-02-26 20:00:08 +0900

    [BOJ] 1920 / 수 찾기 / 실버 4

--- baekjoon/binarysearch/수찾기/s4_01920_수찾기.java
+++ baekjoon/binarysearch/수찾기/s4_01920_수찾기.java
@@ -1,3 +1,5 @@
+package 수찾기;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
---
+++ baekjoon/binarysearch/수찾기/수찾기.java
@@ -0,0 +1,63 @@
+package 수찾기;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 수찾기 {
+
+    static int N;
+    static int[] nums;
+    static int M;
+
+    public static void main(String[] args) throws IOException {
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(
+                new FileReader("baekjoon/binarysearch/수찾기/s4_01920.txt")
+        );
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        nums = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+
+        M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int m = Integer.parseInt(st.nextToken());
+            bSearch(m);
+        }
+    }
+
+    public static void bSearch(int goal) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+
+        while (left <= right) {
+            mid = (left + right) / 2;
+            int num = nums[mid];
+
+            if (num == goal) {
+                System.out.println("1");
+                return;
+            } else if (num < goal) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        System.out.println("0");
+    }
+}
