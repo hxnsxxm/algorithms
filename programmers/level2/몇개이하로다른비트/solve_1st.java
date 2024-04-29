b52e2355c0591abbfc64827bfc47e2d84af0a2ec
hxnsxxm <hansaemj20@gmail.com>
2024-04-29 09:18:11 +0900

    [PGS] 2개 이하로 다른 비트 / 레벨 2

---
+++ programmers/level2/몇개이하로다른비트/solve_1st.java
@@ -0,0 +1,14 @@
+package level2.몇개이하로다른비트;
+
+public class solve_1st {
+    class Solution {
+        public long[] solution(long[] numbers) {
+            long[] answer = numbers.clone();
+            for (int i = 0; i < numbers.length; i++) {
+                answer[i]++; // x보다 큰수로 만든다.
+                answer[i] += (answer[i] ^ numbers[i]) >> 2;
+            }
+            return answer;
+        }
+    }
+}
